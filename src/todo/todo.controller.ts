cb4ec783b699b2f3e7583c8e9086c1d4f129f5cb
ZivWu <zivwu@star-link.tech>
2025-02-19 11:54:21 +0800

    [Feat] 新增todoList query

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { TodoService, Todo as tTodo } from './todo.service';
 
 @Controller('todo')
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
   @Get()
-  getTodos(): tTodo[] {
-    return this.todoService.getTodos();
+  getTodos(
+    @Query() params: { id?: number; limit?: number; skip?: number },
+  ): tTodo[] {
+    const { id, limit = 10, skip = 0 } = params;
+    return this.todoService.getTodos(id, limit, skip);
   }
 
   @Get(':id')
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -10,12 +10,21 @@ const todoList: Todo[] = [
     id: 1,
     subject: 'study',
   },
+  {
+    id: 2,
+    subject: 'work',
+  },
+  {
+    id: 3,
+    subject: 'sport',
+  },
 ];
 
 @Injectable()
 export class TodoService {
-  getTodos(): Todo[] {
-    return todoList;
+  getTodos(id: number | undefined, limit: number, skip: number): Todo[] | [] {
+    const filteredList = id ? todoList.filter((t) => t.id == id) : todoList;
+    return filteredList.slice(skip, limit);
   }
 
   getTodo(id: number): Todo | object {
